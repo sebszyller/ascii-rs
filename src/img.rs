9c068a2307741d5c3ce5d642842c3f808d8462a5
Sebastian Szyller <github@sebszyller.com>
2025-01-27 13:03:09 +0200

    feat: print in colour

---
+++ src/img.rs
@@ -0,0 +1,36 @@
+use image::{DynamicImage, ImageReader, Pixels};
+use std::fs::File;
+use std::io::BufReader;
+
+use anyhow::{Context, Result};
+use colored::Colorize;
+
+pub fn read_image(img_path: &str) -> Result<DynamicImage> {
+    let file = File::open(img_path)
+        .with_context(|| format!("Failed to read image from img_path: {}", img_path))?; // NOTE: we don't want to recover from this
+
+    let reader = BufReader::new(file);
+    let img = ImageReader::new(reader)
+        .with_guessed_format()
+        .with_context(|| format!("Could not guess format"))? // NOTE: we don't want to recover from this
+        .decode()
+        .with_context(|| format!("Failed to decode image"))?; // NOTE: we don't want to recover from this
+
+    Ok(img)
+}
+
+pub fn print_pixels(pixels: Pixels<DynamicImage>) {
+    for (x, y, pixel) in pixels {
+        let formatted = format!("Pixel at ({}, {}): {:?}", x, y, pixel);
+        let colored = formatted.on_truecolor(pixel[0], pixel[1], pixel[2]);
+        println!("{}", colored);
+    }
+}
+
+pub fn print_img_details(img: &DynamicImage) {
+    let width = img.width();
+    let height = img.height();
+
+    println!("Image loaded successfully!");
+    println!("Dimensions: {}x{}", width, height);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-mod image;
+mod img;
 
+use ::image::GenericImageView;
 use anyhow::Result;
 
 fn main() -> Result<()> {
     let path = "test_inputs/church.png";
-    image::print_img_details(path)?;
+    let img = img::read_image(path)?;
+
+    img::print_pixels(img.pixels());
+    img::print_img_details(&img);
     Ok(())
 }
