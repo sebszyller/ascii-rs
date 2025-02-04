62e459aeee79bf24d98972fbcfebc6194f40a9a6
Sebastian Szyller <github@sebszyller.com>
2025-02-04 22:35:04 +0200

    chore: added img tests

--- src/img.rs
+++ src/img.rs
@@ -18,7 +18,6 @@ pub fn read_image(img_path: &str) -> Result<DynamicImage> {
     Ok(img)
 }
 
-// TODO: new tests for this
 pub fn downsize(img: &DynamicImage, width: u32, height: u32) -> Result<DynamicImage> {
     if width > img.width() {
         return Err(anyhow::anyhow!(
@@ -40,5 +39,44 @@ pub fn downsize(img: &DynamicImage, width: u32, height: u32) -> Result<DynamicIm
 pub fn print_pixel_values(pixels: Pixels<DynamicImage>) {
     for (x, y, pixel) in pixels {
         println!("Pixel at ({}, {}): {:?}", x, y, pixel);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_downsize() {
+        let img = DynamicImage::new_rgb8(100, 100);
+        let result = downsize(&img, 50, 50);
+        assert!(result.is_ok());
+        let resized = result.unwrap();
+        assert_eq!(resized.width(), 50);
+        assert_eq!(resized.height(), 50);
+    }
+
+    #[test]
+    fn test_downsize_long_edge() {
+        let img = DynamicImage::new_rgb8(100, 100);
+        let result = downsize(&img, 50, 100);
+        assert!(result.is_ok());
+        let resized = result.unwrap();
+        assert_eq!(resized.width(), 50);
+        assert_eq!(resized.height(), 50);
+    }
+
+    #[test]
+    fn test_downsize_width_error() {
+        let img = DynamicImage::new_rgb8(100, 100);
+        let result = downsize(&img, img.width() + 1, img.height());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_downsize_height_error() {
+        let img = DynamicImage::new_rgb8(100, 100);
+        let result = downsize(&img, img.width(), img.height() + 1);
+        assert!(result.is_err());
     }
 }
